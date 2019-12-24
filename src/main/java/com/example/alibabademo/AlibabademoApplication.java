e94a2407066d7fffad2a3c0efa3b5af57edd5e55
cfrzjj <755042831@qq.com>
2019-12-24 13:59:41 +0800

    Initial commit

---
+++ src/main/java/com/example/alibabademo/AlibabademoApplication.java
@@ -0,0 +1,19 @@
+package com.example.alibabademo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class AlibabademoApplication {
+
+    public static void main(String[] args) {
+        /*SpringApplication.run(AlibabademoApplication.class, args);*/
+
+        ConfigurableApplicationContext applicationContext = SpringApplication.run(AlibabademoApplication.class, args);
+        String userName = applicationContext.getEnvironment().getProperty("user.name");
+        String userAge = applicationContext.getEnvironment().getProperty("user.age");
+        System.err.println("user name :"+userName+"; age: "+userAge);
+    }
+
+}
